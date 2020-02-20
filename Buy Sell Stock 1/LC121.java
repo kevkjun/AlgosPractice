92e1bce9e8b43c4e7e177a668a1eef74f609dcc5
Kevin <kevinkjun@gmail.com>
2020-02-19 22:29:32 -0600

    Feb 19 - LIS is incredibly hard

--- Buy Sell Stock 1/LC121.java
+++ Buy Sell Stock 1/LC121.java
@@ -26,4 +26,29 @@ class Solution {
 
     // Can be done using Kadane's Algorithm which solves the max subarray problem
     // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/39038/Kadane's-Algorithm-Since-no-one-has-mentioned-about-this-so-far-%3A)-(In-case-if-interviewer-twists-the-input)
+
+    // Kadane's Algorithm
+    /*
+    General premise is that we are looking for local optimums at each point (currMax). We are seeing if currMax can increase at all at each step.
+    currMax could possibly increase if the newly seen price is greater than the price we'd seen previously (the second case in the Math.max formula).
+
+    currMax is being compared with 0 at every point because if currMax decreases, that means that we should choose that point to buy the stock instead
+    of whatever point we'd bought it earlier if currMax dips to below 0 because of the change in the stock.
+
+    max is always the max that we have seen so far. 
+    */
+    // Faster than 83%
+    // Less than 5%
+    public int maxProfit1(int[] prices) {
+        int len = prices.length;
+        if (len == 0) return 0;
+        else if (len == 1) return 0;
+        
+        int max = 0, currMax = 0;
+        for (int i = 1; i < len; i++) {
+            currMax = Math.max(0, currMax += prices[i] - prices[i-1]);
+            max = Math.max(max, currMax);
+        }
+        return max;
+    }
 }
